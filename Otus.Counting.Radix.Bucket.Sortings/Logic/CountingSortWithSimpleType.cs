1a2f73d66f9ed76897529ad9fcd182d7b52692a6
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-07-05 22:26:28 +0300

    implemented Counting Sort (draft with simple types)

---
+++ Otus.Counting.Radix.Bucket.Sortings/Logic/CountingSortWithSimpleType.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Otus.Counting.Radix.Bucket.Sortings.Logic
+{
+    public class CountingSortWithSimpleType
+    {
+        private int[] _array;
+
+
+        public CountingSortWithSimpleType(int[] array)
+        {
+            _array = array;
+        }
+
+
+        public int[] Run()
+        {
+            var uniqueValuesArray = FormUniqueValuesArray();
+
+            return PushElementsToNewArray(uniqueValuesArray);
+        }
+
+
+        #region Support methods
+
+        private Dictionary<int, int> FormUniqueValuesArray()
+        {
+            var uniqueValuesArray = new Dictionary<int, int>();
+
+            for (var i = 0; i < _array.Length; i++)
+            {
+                var currentElement = _array[i];
+
+                if (uniqueValuesArray.ContainsKey(currentElement))
+                {
+                    uniqueValuesArray[currentElement]++;
+                }
+                else
+                {
+                    uniqueValuesArray.Add(currentElement, 1);
+                }
+            }
+
+            for (var i = 1; i < uniqueValuesArray.Count; i++)
+            {
+                var previousElement = uniqueValuesArray.ElementAt(i - 1);
+                var currentElement = uniqueValuesArray.ElementAt(i);
+                
+                uniqueValuesArray[currentElement.Key] = currentElement.Value + previousElement.Value;
+            }
+
+            return uniqueValuesArray;
+        }
+
+        private int[] PushElementsToNewArray(Dictionary<int, int> uniqueValuesArray)
+        {
+            var newArray = new int[_array.Length];
+
+            for (var i = _array.Length - 1; i >= 0; i--)
+            {
+                var currentElement = _array[i];
+                var indexUpperBound = --uniqueValuesArray[currentElement];
+                
+                newArray[indexUpperBound] = currentElement;
+            }
+
+            return newArray;
+        }
+
+        #endregion
+    }
+}
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -5,17 +5,27 @@ namespace Tests
 {
     public class Tests
     {
-        private int[] _initialArray = { 32, 95, 16, 82, 24, 66, 35, 19, 75, 54, 40, 43, 93, 68 };
-        private int[] _sortedArray = { 16, 19, 24, 32, 35, 40, 43, 54, 66, 68, 75, 82, 93, 95 };
+        //private int[] _initialArray = { 32, 95, 16, 82, 24, 66, 35, 19, 75, 54, 40, 43, 93, 68 };
+        //private int[] _sortedArray = { 16, 19, 24, 32, 35, 40, 43, 54, 66, 68, 75, 82, 93, 95 };
 
-        //private int[] _initialArray = { 2, 7, 3, 1, 5, 9, 4 };
-        //private int[] _sortedArray = { 1, 2, 3, 4, 5, 7, 9 };
+        private int[] _initialArray = { 0, 1, 0, 2, 0, 3, 0, 4, 2, 1, 3, 4, 0, 2, 3, 4 };
+        private int[] _sortedArray = { 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4 };
 
 
         [Test]
         public void Can_Sort_Via_Bucket_Sort()
         {
             var sorter = new BucketSort(GetInitialArray());
+
+            var sortedArray = sorter.Run();
+
+            CollectionAssert.AreEqual(_sortedArray, sortedArray);
+        }
+
+        [Test]
+        public void Can_Sort_simple_Types_Via_Counting_Sort()
+        {
+            var sorter = new CountingSortWithSimpleType(GetInitialArray());
 
             var sortedArray = sorter.Run();
 
